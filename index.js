78e172ac4f0be083ac810f676a810deeeba525f6
cxwcfea <cxwcfea@163.com>
2018-05-06 15:35:40 +0800

    update

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('run');
+const mysqlx = require('@mysqlx/xdevapi');
+const assert = require('assert');
+
+const dbName = 'myproject';
+
+const url = `mysqlx://root@my-db:3306/${dbName}`;
+
+const main = async () => {
+  try {
+    const session = await mysqlx.getSession(url);
+    console.log('Successful server connection');
+
+    const db = session.getSchema(dbName);
+
+    session.close();
+  } catch (err) {
+    console.error(err.stack);
+    process.exit(1);
+  }
+}
+
+main();
